09ae793c81b1b7b17f82fede8cbb35bc59dda328
GreenTheColour1 <contact@camerongreen.ca>
2024-09-29 05:13:51 -0400

    add proton ge

--- system/app/steam.nix
+++ system/app/steam.nix
@@ -2,8 +2,13 @@
 
 {
   hardware.graphics.enable32Bit = true;
-  programs.steam.enable = true;
-  programs.steam.protontricks.enable = true;
-  programs.steam.protontricks.package = pkgs.protontricks;
+  programs.steam = {
+    enable = true;
+
+    extraCompatPackages = with pkgs; [ proton-ge-bin ];
+
+    protontricks.enable = true;
+    protontricks.package = pkgs.protontricks;
+  };
   environment.systemPackages = [ pkgs.steam ];
 }
