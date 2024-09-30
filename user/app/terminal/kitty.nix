ba23500c025e4091340bf5e06faf3107016ec6e1
GreenTheColour1 <contact@camerongreen.ca>
2024-09-30 17:43:10 -0400

    kitty conf

--- user/app/terminal/kitty.nix
+++ user/app/terminal/kitty.nix
@@ -6,7 +6,9 @@
   ];
   programs.kitty.enable = true;
   programs.kitty.settings = {
-    background_opacity = lib.mkForce "0.85";
     modify_font = "cell_width 90%";
+    enable_audio_bell = "no";
+
+    sync_to_monito = "no";
   };
 }
