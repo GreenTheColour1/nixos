1ee1cbb9d42cd42b75357cefacc46b6e95cbd59d
GreenTheColour1 <contact@camerongreen.ca>
2024-10-06 06:43:28 -0400

    more libvert

--- system/app/virt-manager.nix
+++ system/app/virt-manager.nix
@@ -1,6 +1,27 @@
 { pkgs, ... }:
 {
-  virtualisation.libvirtd.enable = true;
+  virtualisation.libvirtd = {
+    enable = true;
+    qemu = {
+      package = pkgs.qemu_kvm;
+      runAsRoot = true;
+      swtpm.enable = true;
+      ovmf = {
+        enable = true;
+        packages = [
+          (pkgs.OVMF.override {
+            secureBoot = true;
+            tpmSupport = true;
+          }).fd
+        ];
+      };
+      vhostUserPackages = with pkgs; [
+        virtio-win
+        virtiofsd
+      ];
+    };
+  };
+
   programs.virt-manager.enable = true;
   virtualisation.spiceUSBRedirection.enable = true;
 
