f72d54206eefdd6a78e96c16614d42005be0aae1
GreenTheColour1 <contact@camerongreen.ca>
2024-10-06 01:15:35 -0400

    add virtio-win

--- system/app/virt-manager.nix
+++ system/app/virt-manager.nix
@@ -1,6 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   virtualisation.libvirtd.enable = true;
   programs.virt-manager.enable = true;
   virtualisation.spiceUSBRedirection.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    virtio-win
+  ];
 }
