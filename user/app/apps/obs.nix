d3779bffaf04bdc4367c2506e4b3c4368c7f20c1
GreenTheColour1 <contact@camerongreen.ca>
2024-10-08 07:05:38 -0400

    remove obsolete obs plugin

--- user/app/apps/obs.nix
+++ user/app/apps/obs.nix
@@ -3,11 +3,9 @@
   programs.obs-studio = {
     enable = true;
     plugins = with pkgs.obs-studio-plugins; [
-      wlrobs
       obs-pipewire-audio-capture
       obs-vkcapture
       obs-tuna
     ];
   };
 }
-
