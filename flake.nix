99d14721cb471c853bccf548d1fe36863b77d915
GreenTheColour1 <contact@camerongreen.ca>
2024-11-11 19:04:56 -0500

    add spicetify

--- flake.nix
+++ flake.nix
@@ -111,7 +111,7 @@
 
     stylix.url = "github:danth/stylix";
 
-    zen-browser.url = "github:GreenTheColour1/zen-browser-flake";
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
     zen-browser.inputs.nixpkgs.follows = "nixpkgs";
 
     aagl.url = "github:ezKEa/aagl-gtk-on-nix";
@@ -122,5 +122,8 @@
     xremap-flake.url = "github:xremap/nix-flake";
 
     nix-minecraft.url = "github:Infinidoge/nix-minecraft";
+
+    spicetify-nix.url = "github:Gerg-L/spicetify-nix";
+    spicetify-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 }
--- user/app/media/spotify.nix
+++ user/app/media/spotify.nix
@@ -1,6 +1,21 @@
-{ pkgs, ... }:
+{ pkgs, inputs, ... }:
 {
+  imports = [ inputs.spicetify-nix.homeManagerModules.default ];
   home.packages = with pkgs; [
-    spotify
+    # spotify
   ];
+
+  programs.spicetify =
+    let
+      spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+    in
+    {
+      enable = true;
+      enabledExtensions = with spicePkgs.extensions; [
+        shuffle
+        fullAppDisplay
+      ];
+      theme = spicePkgs.themes.dribbblish;
+      colorScheme = "gruvbox-material-dark";
+    };
 }
