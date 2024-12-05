7ec9cb9d1f3ab5b6de91aece5d0e137f093bccd1
GreenTheColour1 <contact@camerongreen.ca>
2024-12-05 00:24:09 -0500

    rofi update

--- user/wm/wayland/rofi.nix
+++ user/wm/wayland/rofi.nix
@@ -3,5 +3,15 @@
   programs.rofi = {
     enable = true;
     package = pkgs.rofi-wayland;
+    extraConfig = {
+      modi = "drun";
+      columns = 1;
+      lines = 5;
+      show-icons = true;
+      terminal = "kitty";
+      drun-display-format = "{icon} {name}";
+      seperator-style = "style";
+      matching = "fuzzy";
+    };
   };
 }
