d695735d5f7d2209a032bad87020b8a8931190a2
GreenTheColour1 <contact@camerongreen.ca>
2024-12-29 20:24:53 -0500

    rename

---
+++ user/app/social/discord.nix
@@ -0,0 +1,15 @@
+{ pkgs, inputs, ... }:
+{
+  imports = [ inputs.nixcord.homeManagerModules.nixcord ];
+
+  programs.nixcord = {
+    enable = true;
+    # config = {
+    #   themeLinks = [
+    #     "https://raw.githubusercontent.com/refact0r/midnight-discord/refs/heads/master/flavors/midnight-vencord.theme.css"
+    #   ];
+    #
+    #   frameless = true;
+    # };
+  };
+}
