6f5ead0ef6616e2b7aab170825a11ef105729793
GreenTheColour1 <contact@camerongreen.ca>
2025-01-24 19:29:32 -0500

    switch to latest kernel

--- profiles/personal/configuration.nix
+++ profiles/personal/configuration.nix
@@ -66,6 +66,8 @@
   };
   boot.loader.efi.canTouchEfiVariables = true;
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
   boot.extraModulePackages = with config.boot.kernelPackages; [ xpadneo ];
 
   boot.kernelParams = [
