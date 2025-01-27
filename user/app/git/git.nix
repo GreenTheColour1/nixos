7513b70f9793391501c5651c74ea273d0a79cace
GreenTheColour1 <contact@camerongreen.ca>
2025-01-26 22:13:59 -0500

    remove old files

--- user/app/git/git.nix
+++
@@ -1,8 +0,0 @@
-{ config, pkgs, userSettings, ... }:
-
-{
-  home.packages = [ pkgs.git pkgs.lazygit ];
-  programs.git.enable = true;
-  programs.git.userName = userSettings.name;
-  programs.git.userEmail = userSettings.email;
-}
