24cf8c4423621fdf644c1fc6a70840ac874b14e5
GreenTheColour1 <contact@camerongreen.ca>
2025-02-28 19:02:43 -0500

    proton-vpn: add helper script for port forwarding

--- modules/programs/proton-vpn.nix
+++ modules/programs/proton-vpn.nix
@@ -11,5 +11,15 @@ delib.module {
   nixos.ifEnabled.environment.systemPackages = with pkgs; [
     protonvpn-gui
     libnatpmp
+    (writeShellScriptBin "pvpnpf" ''
+      while true; do
+      	date
+      	natpmpc -a 1 0 udp 60 -g 10.2.0.1 && natpmpc -a 1 0 tcp 60 -g 10.2.0.1 || {
+      		echo -e "ERROR with natpmpc command \a"
+      		break
+      	};
+      	sleep 45
+      done
+    '')
   ];
 }
