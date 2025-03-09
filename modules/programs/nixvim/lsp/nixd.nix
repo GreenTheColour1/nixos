6594503fee811ff6720a0f8202baa53acc439277
GreenTheColour1 <contact@camerongreen.ca>
2025-03-09 01:36:00 -0500

    test: add denix options to nixd

--- modules/programs/nixvim/lsp/nixd.nix
+++ modules/programs/nixvim/lsp/nixd.nix
@@ -1,27 +1,37 @@
-{ delib, pkgs, lib, ... }:
+{
+  delib,
+  pkgs,
+  lib,
+  ...
+}:
 let
-  flake = ''(builtins.getFlake "github:GreenTheColour1/nixos)""'';
+  flake = ''(builtins.getFlake "github:GreenTheColour1/nixos)"'';
 in
 delib.module {
   name = "programs.nixvim.lsp.nixd";
 
   options = delib.singleEnableOption true;
 
-  home.ifEnabled = { myconfig, ... }: {
-    programs.nixvim.plugins.lsp.servers.nixd = {
-      enable = true;
-      settings = {
-        formatting.command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
+  home.ifEnabled =
+    { myconfig, ... }:
+    {
+      programs.nixvim.plugins.lsp.servers.nixd = {
+        enable = true;
+        settings = {
+          formatting.command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
 
-        options = {
-          nixos = {
-            expr = "${flake}.nixosConfigurations.nixos-fishy.options"; # FIXME
-          };
-          home_manager = {
-            expr = "${flake}.homeConfigurations.${myconfig.constants.username}@nixos-fishy.options"; #FIXME
+          options = {
+            nixos = {
+              expr = "${flake}.nixosConfigurations.nixos-fishy.options"; # FIXME
+            };
+            home_manager = {
+              expr = "${flake}.homeConfigurations.${myconfig.constants.username}@nixos-fishy.options"; # FIXME
+            };
+            denix = {
+              expr = "(builtins.getFlake \"github:yunfachi/denix\").lib.options";
+            };
           };
         };
       };
     };
-  };
 }
