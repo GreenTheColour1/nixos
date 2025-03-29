11396d04cf629fcdf312b5630b30b161a5e0e131
GreenTheColour1 <contact@camerongreen.ca>
2025-03-29 14:32:56 -0400

    hyprland: update hyprland rules

--- modules/programs/hyprland/rules.nix
+++ modules/programs/hyprland/rules.nix
@@ -2,19 +2,17 @@
 delib.module {
   name = "programs.hyprland";
 
-  home.ifEnabled = {myconfig, ...}: {
-    wayland.windowManager.hyprland.settings = {
-      windowrule = [
-        "opacity 0.7 override,^(kitty)$"
-        "float,^(waypaper)$"
-        "workspace 4, ^(vesktop)$"
-      ];
-      
-      windowrulev2 = [
-        "float, class:(clipse)"
-        "size 622 652, class:(clipse)"
-        "maxsize 400 1000, class:(com.chatterino.)"
-      ];
+  home.ifEnabled =
+    { myconfig, ... }:
+    {
+      wayland.windowManager.hyprland.settings = {
+        windowrule = [
+          "opacity 0.7 override,class:^(kitty)$"
+          "float,class:^(waypaper)$"
+          "workspace 4, class:^(vesktop)$"
+          "float, class:(clipse)"
+          "size 622 652, class:(clipse)"
+        ];
+      };
     };
-  };
 }
