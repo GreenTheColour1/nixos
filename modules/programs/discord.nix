4467508d1b8e8a4dd475f7eba86e55b0373e7a4a
GreenTheColour1 <contact@camerongreen.ca>
2025-05-01 15:40:08 -0400

    change from vesttop to nixvim

--- modules/programs/discord.nix
+++ modules/programs/discord.nix
@@ -1,10 +1,25 @@
-{ delib, host, pkgs, ... }:
+{
+  delib,
+  host,
+  pkgs,
+  inputs,
+  ...
+}:
 delib.module {
   name = "programs.discord";
 
   options = delib.singleEnableOption host.isDesktop;
 
+  home.always.imports = [ inputs.nixcord.homeModules.nixcord ];
+
   home.ifEnabled = {
-    home.packages = with pkgs; [ vesktop ];
+    # home.packages = with pkgs; [ vesktop ];
+
+    programs.nixcord = {
+      enable = true;
+      config = {
+        frameless = true;
+      };
+    };
   };
 }
