1a55bb38a05d352a9b0c7369d0ee1e61ab6f2bb7
GreenTheColour1 <contact@camerongreen.ca>
2025-05-01 15:45:13 -0400

    boot.nix formatting

--- modules/toplevel/boot.nix
+++ modules/toplevel/boot.nix
@@ -11,41 +11,35 @@ delib.module {
 
   options.boot = with delib; {
     enable = boolOption true;
-
-    mode = enumOption [ "uefi" "legacy" ] (
-      if builtins.pathExists /sys/firmware/efi/efivars then "uefi" else "legacy"
-    );
   };
 
   nixos.always.imports = [ inputs.lanzaboote.nixosModules.lanzaboote ];
 
-  nixos.ifEnabled =
-    { cfg, ... }:
-    {
-      environment.systemPackages = with pkgs; [
-        sbctl
-      ];
-      boot.kernelPackages = pkgs.linuxPackages_latest;
+  nixos.ifEnabled = {
+    environment.systemPackages = with pkgs; [
+      sbctl
+    ];
+    boot.kernelPackages = pkgs.linuxPackages_latest;
 
-      boot.loader = {
-        efi = {
-          canTouchEfiVariables = true;
-        };
-        systemd-boot = {
-          enable = lib.mkForce false;
-        };
+    boot.loader = {
+      efi = {
+        canTouchEfiVariables = true;
       };
-
-      boot.lanzaboote = {
-        enable = true;
-        pkiBundle = "/var/lib/sbctl";
+      systemd-boot = {
+        enable = lib.mkForce false;
       };
-
-      # boot.extraModulePackages = with config.boot.kernelPackages; [ xpadneo ];
-
-      boot.kernelParams = [
-        "amd_pstate=active"
-        "initcall_blacklist=simpledrm_platform_driver_init" # Fix ghost monitor
-      ];
     };
+
+    boot.lanzaboote = {
+      enable = true;
+      pkiBundle = "/var/lib/sbctl";
+    };
+
+    # boot.extraModulePackages = with config.boot.kernelPackages; [ xpadneo ];
+
+    boot.kernelParams = [
+      "amd_pstate=active"
+      "initcall_blacklist=simpledrm_platform_driver_init" # Fix ghost monitor
+    ];
+  };
 }
