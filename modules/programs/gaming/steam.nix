1145c043debc16220ba4368205b0bc1f9830330b
GreenTheColour1 <contact@camerongreen.ca>
2025-05-13 16:58:15 -0400

    test global env var for steam games

--- modules/programs/gaming/steam.nix
+++ modules/programs/gaming/steam.nix
@@ -1,13 +1,25 @@
-{ delib, host, pkgs, ... }:
+{
+  delib,
+  host,
+  pkgs,
+  ...
+}:
 delib.module {
   name = "programs.steam";
 
   options = delib.singleEnableOption host.isDesktop;
 
-  nixos.ifEnabled.environment.systemPackages = [pkgs.steam];
+  nixos.ifEnabled.environment.systemPackages = [ pkgs.steam ];
 
   nixos.ifEnabled.programs.steam = {
     enable = true;
+
+    package = pkgs.steam.override {
+      extraEnv = {
+        OBS_VKCAPTURE = 1;
+      };
+    };
+
     protontricks.enable = true;
   };
 }
