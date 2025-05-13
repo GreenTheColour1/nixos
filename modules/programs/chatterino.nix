a547170b4a0500078af24c8a4f545829e09659ec
GreenTheColour1 <contact@camerongreen.ca>
2025-05-13 16:56:49 -0400

    re-enable chatterino

--- modules/programs/chatterino.nix
+++ modules/programs/chatterino.nix
@@ -8,6 +8,7 @@ delib.module {
   name = "programs.chatterino";
 
   options = delib.singleEnableOption host.isDesktop;
+  # options = delib.singleEnableOption false;
 
   home.ifEnabled.home.packages = with pkgs; [ chatterino7 ];
 }
