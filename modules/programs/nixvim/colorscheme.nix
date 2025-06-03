66c78e761b2da45a93064ee1d31abaa2e41b31a8
GreenTheColour1 <contact@camerongreen.ca>
2025-06-03 17:47:14 -0400

    nixvim: enable term colours

--- modules/programs/nixvim/colorscheme.nix
+++ modules/programs/nixvim/colorscheme.nix
@@ -3,6 +3,11 @@ delib.module {
   name = "programs.nixvim";
 
   home.ifEnabled.programs.nixvim = {
-    colorschemes.gruvbox.enable = true;
+    colorschemes.gruvbox = {
+      enable = true;
+      settings = {
+        terminal_colors = true;
+      };
+    };
   };
 }
