2c91727cd8a2a92ee5eca7004505d4530430520f
GreenTheColour1 <contact@camerongreen.ca>
2025-06-10 17:14:09 -0400

    add libratbag + piper

--- modules/programs/hardware/logitech.nix
+++ modules/programs/hardware/logitech.nix
@@ -1,13 +1,16 @@
-{ delib, host, ... }:
+{
+  delib,
+  host,
+  pkgs,
+  ...
+}:
 delib.module {
   name = "programs.hardware.logitech";
 
   options = delib.singleEnableOption host.isDesktop;
 
-  nixos.ifEnabled.hardware.logitech = {
-    wireless = {
-      enable = true;
-      enableGraphical = true;
-    };
+  nixos.ifEnabled = {
+    services.ratbag.enable = true;
+    environemnt.systemPackages = with pkgs; [ piper ];
   };
 }
