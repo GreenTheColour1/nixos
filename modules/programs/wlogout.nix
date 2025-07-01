f7aeb6817f39d131fe9a376669473dee5ca857ad
GreenTheColour1 <dev@camerongreen.ca>
2025-07-01 18:03:17 -0400

    wlogout: remove uneeded comment

--- modules/programs/wlogout.nix
+++ modules/programs/wlogout.nix
@@ -23,7 +23,7 @@ delib.module {
         }
         {
           "label" = "windows";
-          "action" = "systemctl reboot --boot-loader-entry=auto-windows"; # this should reboot into windows assuming windows the second entry
+          "action" = "systemctl reboot --boot-loader-entry=auto-windows";
           "keybind" = "w";
         }
         {
