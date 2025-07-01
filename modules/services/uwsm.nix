3e5d26bb5332ada21479c18038f8638b145063c8
GreenTheColour1 <dev@camerongreen.ca>
2025-07-01 18:08:34 -0400

    uwsm: fix typo

--- modules/services/uwsm.nix
+++ modules/services/uwsm.nix
@@ -8,7 +8,7 @@
 delib.module {
   name = "services.uwsm";
 
-  options = delib.simpleEnableOption host.isDesktop;
+  options = delib.singleEnableOption host.isDesktop;
 
   nixos.ifEnabled =
     { myconfig, ... }:
