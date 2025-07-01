cdd03eb38992e682fbf073a6e82437eba77a3f09
GreenTheColour1 <dev@camerongreen.ca>
2025-07-01 17:59:50 -0400

    apt: add keybinds

--- modules/programs/gaming/apt.nix
+++ modules/programs/gaming/apt.nix
@@ -2,6 +2,7 @@
   delib,
   inputs,
   host,
+  lib,
   ...
 }:
 delib.module {
@@ -9,7 +10,14 @@ delib.module {
 
   options = delib.singleEnableOption host.isDesktop;
 
-  home.ifEnabled.home.packages = [
-    inputs.greenpkgs.packages.x86_64-linux.awakened-poe-trade
-  ];
+  home.ifEnabled =
+    { myconfig, ... }:
+    {
+      home.packages = [ inputs.greenpkgs.packages.x86_64-linux.awakened-poe-trade ];
+
+      wayland.windowManager.hyprland.settings.binds = lib.mkIf myconfig.programs.hyprland.enable [
+        "CTRL, D, pass, pid:14260"
+        ", F5, pass, pid:14260"
+      ];
+    };
 }
