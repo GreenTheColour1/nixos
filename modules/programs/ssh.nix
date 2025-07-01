4058f866d9a7693d1bb2887587b83c64d1e7c3cf
GreenTheColour1 <dev@camerongreen.ca>
2025-07-01 18:01:43 -0400

    ssh: github ssh rules

--- modules/programs/ssh.nix
+++ modules/programs/ssh.nix
@@ -6,10 +6,13 @@ delib.module {
 
   home.ifEnabled.programs.ssh = {
     enable = true;
+    addKeysToAgent = "yes";
 
     matchBlocks = {
       "git" = {
         host = "github.com";
+        user = "git";
+        hostname = "github.com";
         identitiesOnly = true;
         identityFile = [
           "~/.ssh/id_git"
