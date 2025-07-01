ce17909f030751d618f98ec1127b149a809d4dfe
GreenTheColour1 <dev@camerongreen.ca>
2025-07-01 18:05:42 -0400

    nvidia: fix suspend

--- modules/toplevel/nvidia.nix
+++ modules/toplevel/nvidia.nix
@@ -23,6 +23,7 @@ delib.module {
 
     hardware.nvidia = {
       modesetting.enable = true;
+      powerManagement.enable = true;
       open = true;
       package = config.boot.kernelPackages.nvidiaPackages.latest;
     };
@@ -35,6 +36,27 @@ delib.module {
       VDPAU_DRIVER = "nvidia";
       EGL_PLATFORM = "wayland";
       # __GL_GSYNC_ALLOWED = 1;
+    };
+
+    # Needed or else system failes to wake up after suspend
+    systemd.services."nvidia-unload" = {
+      description = "Unload NVIDIA modules before suspend";
+      before = [ "sleep.target" ];
+      wantedBy = [ "sleep.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.kmod}/bin/modprobe -r nvidia_drm nvidia_modeset nvidia_uvm nvidia";
+      };
+    };
+
+    systemd.services."nvidia-reload" = {
+      description = "Reload NVIDIA modules after resume";
+      after = [ "suspend.target" ];
+      wantedBy = [ "suspend.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        ExecStart = "${pkgs.kmod}/bin/modprobe nvidia nvidia_modeset nvidia_uvm nvidia_drm";
+      };
     };
   };
 }
