cd95cd63cffba4d8708624268e77d6921fb37f1e
GreenTheColour1 <dev@camerongreen.ca>
2025-08-12 21:12:58 -0400

    fix spelling error

--- modules/toplevel/nvidia-prime.nix
+++ modules/toplevel/nvidia-prime.nix
@@ -7,7 +7,7 @@ delib.module {
   nixos.ifEnabled = {
     hardware.nvidia.prime = {
       offload.enable = true;
-      offlaod.enableOffloadCmd = true;
+      offload.enableOffloadCmd = true;
 
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:1:0:0";
