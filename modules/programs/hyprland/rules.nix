656a14467888a2f0aac4ed631df0b9a63e3977de
GreenTheColour1 <dev@camerongreen.ca>
2026-02-19 16:18:51 -0500

    update hyprland rules

--- modules/programs/hyprland/rules.nix
+++ modules/programs/hyprland/rules.nix
@@ -22,18 +22,6 @@ delib.module {
           # "opacity 0.2, title:(HearthstoneOverlay)"
           # "stayfocused, title:^(Hearthstone)$"
         ];
-        windowrulev2 = [
-          "float, title:(HearthstoneOverlay)"
-          "center, title:(HearthstoneOverlay)"
-          "noblur, title:(HearthstoneOverlay)"
-          "noshadow, title:(HearthstoneOverlay)"
-          "noanim, title:(HearthstoneOverlay)"
-          "nodim, title:(HearthstoneOverlay)"
-          "nofocus, title:(HearthstoneOverlay)"
-          "noinitialfocus, title:(HearthstoneOverlay)"
-          "opacity 0.5, title:(HearthstoneOverlay)"
-          "tile, title:(Hearthstone)"
-        ];
       };
     };
 }
