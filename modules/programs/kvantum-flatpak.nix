0aa9265e956cebb9ce578bf6da1d3bfdbf8aa6c0
GreenTheColour1 <dev@camerongreen.ca>
2026-02-19 16:22:39 -0500

    kvantum: disable

--- modules/programs/kvantum-flatpak.nix
+++ modules/programs/kvantum-flatpak.nix
@@ -7,7 +7,8 @@
 delib.module {
   name = "kvantum-flatpak";
 
-  options = delib.singleEnableOption host.guiFeatured;
+  # options = delib.singleEnableOption host.guiFeatured;
+  options = delib.singleEnableOption false;
 
   myconfig.ifEnabled.services.flatpak.packages = [
     { appId = "org.kde.KStyle.Kvantum"; }
