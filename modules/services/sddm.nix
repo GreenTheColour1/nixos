2232519a76e59e5ac34f6a60c1804d14c8e0e220
GreenTheColour1 <dev@camerongreen.ca>
2026-06-10 17:20:25 -0400

    sddm: cleanup

--- modules/services/sddm.nix
+++ modules/services/sddm.nix
@@ -12,28 +12,12 @@ delib.module {
 
   nixos.ifEnabled =
     { myconfig, ... }:
-    let
-      # sddm-theme = inputs.silentSDDM.packages.${pkgs.system}.default.override {
-      #   theme = "default";
-      # };
-    in
     {
-      # environment.systemPackages = [ sddm-theme ];
       qt.enable = true;
       services.displayManager = {
         sddm = {
           enable = true;
-          package = pkgs.kdePackages.sddm;
           wayland.enable = true;
-          # theme = sddm-theme.pname;
-          # extraPackages = sddm-theme.propagatedBuildInputs;
-          # settings = {
-          #   # required for styling the virtual keyboard
-          #   General = {
-          #     GreeterEnvironment = "QML2_IMPORT_PATH=${sddm-theme}/share/sddm/themes/${sddm-theme.pname}/components/,QT_IM_MODULE=qtvirtualkeyboard";
-          #     InputMethod = "qtvirtualkeyboard";
-          #   };
-          # };
         };
         autoLogin.enable = true;
         autoLogin.user = myconfig.constants.username;
